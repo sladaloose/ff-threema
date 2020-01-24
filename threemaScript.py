d09dd89f4db1c878805754d4297fc71ba7069057
Johannes Weiß <sladaloose@cj-weiss.de>
2020-01-24 21:24:33 +0100

    feat: adding extraction features

--- threemaScript.py
+++ threemaScript.py
@@ -1,6 +1,55 @@
+# coding=UTF-8
+
+import sys
+import re
 
 def helloWorld(text):
     return text
 
+def getFileName(): # extract the second argument
+    return str(sys.argv[1])
 
-print (helloWorld("Hello World"))
+def readFileContent(file):
+    f=open(file, "r")
+    contents = f.read()
+    return contents
+
+def extractSachverhalt(contents):
+    try:
+        found = re.search('Sachverhalt: (.+?)\n', contents).group(1)
+        found = found.strip()
+    except AttributeError:
+        found = ''
+    return found
+
+def extractOrt(contents):
+    try:
+        contents = contents.replace(" :",":")
+        strasse = re.search('Straße: (.+?)Haus-Nr.(.+?)\n', contents).group(1)
+        strasse = strasse.strip()
+        nr = re.search('Haus-Nr.(.+?)\n', contents).group(1)
+        nr = nr.replace(":", "")
+        nr = nr.strip()
+        if nr != "":
+            nr = nr + ' '
+        ort = re.search('Ort: (.+?)\n', contents).group(1)
+        ort = ort.strip()
+        found = strasse + ' ' + nr + ort
+    except AttributeError as err:
+        found = ''
+    return found
+
+def createThreemaMessage(sachverhalt, ort):
+    message = sachverhalt + "\n" + ort
+    return message
+
+def sendThreemaMessage(message):
+    print("sending Threema message: " + message)
+
+
+if __name__ == '__main__':
+    fileContent = readFileContent(getFileName())
+    sachverhalt = extractSachverhalt(fileContent)
+    ort = extractOrt(fileContent)
+    message = createThreemaMessage(sachverhalt, ort)
+    sendThreemaMessage(message)
--- threemaScriptTest.py
+++ threemaScriptTest.py
@@ -1,5 +1,8 @@
+# coding=UTF-8
+
 import unittest
 import threemaScript
+import re
 
 class TestThreemaScript(unittest.TestCase):
 
@@ -7,7 +10,96 @@ class TestThreemaScript(unittest.TestCase):
         value = threemaScript.helloWorld("Hello World")
         self.assertEqual(value, 'Hello World')
 
+    def test_29_6_19(self):
+        text = """Straße: Weiherer Straße Haus-Nr.:
+Ortsteil=
+Ort: 96170 Trabelsdorf - Lisberg
+Objekt:
+Sachverhalt: Technische Hilfe #T2726#VU#mit PKW
+Einsatznummer= T 4.1 190629 1042
+Zusatzinformation1: 1
+***ENDE DER DATEI***"""
+        sachverhalt = threemaScript.extractSachverhalt(text)
+        ort = threemaScript.extractOrt(text)
+        self.assertEqual(sachverhalt, "Technische Hilfe #T2726#VU#mit PKW")
+        self.assertEqual(ort, "Weiherer Straße 96170 Trabelsdorf - Lisberg")
 
+    def test_7_8_19(self):
+        text = """Straße: Amselweg Haus-Nr.: 7
+Ortsteil= Amselweg
+Ort: 96194 Walsdorf - Walsdorf Oberfr
+Objekt:
+Sachverhalt: Brand #B1111#im Gebäude#Dachstuhl
+Einsatznummer= B 4.1 190807 829
+Zusatzinformation1: 1
+***ENDE DER DATEI***"""
+        sachverhalt = threemaScript.extractSachverhalt(text)
+        ort = threemaScript.extractOrt(text)
+        self.assertEqual(sachverhalt, "Brand #B1111#im Gebäude#Dachstuhl")
+        self.assertEqual(ort, "Amselweg 7 96194 Walsdorf - Walsdorf Oberfr")
+
+    def test_4_5_10(self):
+        text = """Straße : Steigerwaldstraße Haus-Nr.: 13
+Ortsteil :
+Ort : 96170 Trabelsdorf - Lisberg
+Objekt : 4.1.2 BA-L FF Feuerwehr Trabelsdorf
+Sachverhalt: Technische Hilfe #T1510#RD#First Responder
+Einsatznummer= T 4.1 190504 700
+Zusatzinformation1.: 1
+***ENDE DER DATEI***"""
+        sachverhalt = threemaScript.extractSachverhalt(text)
+        ort = threemaScript.extractOrt(text)
+        self.assertEqual(sachverhalt, "Technische Hilfe #T1510#RD#First Responder")
+        self.assertEqual(ort, "Steigerwaldstraße 13 96170 Trabelsdorf - Lisberg")
+
+    def test_1_7_19(self):
+        text = """Straße: Seeleite Haus-Nr.: 1
+Ortsteil= Seeleite
+Ort: 96170 Trabelsdorf - Lisberg
+Objekt: 4.1.2 BA-L Hotel Landhotel Altes Kurhaus Trabelsdorf
+Sachverhalt: Technische Hilfe #R8210#Wasserrettung#1 Person in Wassernot
+Einsatznummer= T 4.1 190701 1047
+Zusatzinformation1: 1
+***ENDE DER DATEI***"""
+        sachverhalt = threemaScript.extractSachverhalt(text)
+        ort = threemaScript.extractOrt(text)
+        self.assertEqual(sachverhalt, "Technische Hilfe #R8210#Wasserrettung#1 Person in Wassernot")
+        self.assertEqual(ort, "Seeleite 1 96170 Trabelsdorf - Lisberg")
+
+    def test_6_5_19(self):
+        text = """Straße : Hauptstraße Haus-Nr.:
+Ortsteil : Hauptstraße
+Ort : 96170 Lisberg - Lisberg
+Objekt :
+Sachverhalt: Sonstige 2017-4.1 Hilfeleistung - Unterstützung Polizei
+Einsatznummer= S 4.1 190506 85
+Zusatzinformation1.: 0
+***ENDE DER DATEI***"""
+        sachverhalt = threemaScript.extractSachverhalt(text)
+        ort = threemaScript.extractOrt(text)
+        self.assertEqual(sachverhalt, "Sonstige 2017-4.1 Hilfeleistung - Unterstützung Polizei")
+        self.assertEqual(ort, "Hauptstraße 96170 Lisberg - Lisberg")
+
+    def test_20_12_19(self):
+        text = """Straßez Steigerwaldstraße Haus-NI.:
+Ortsteilz
+Ort: 95l7O Trabelsdort - Lisberg
+Objektz
+Sachverhaltz Brand #Bl5l5#VeIkehI#LKW / Bus innerorts
+Einsatznummer: B 4.l l9l22O l257
+Zusatzinformation1c l
+***ENDE DER DATEI***"""
+        sachverhalt = threemaScript.extractSachverhalt(text)
+        ort = threemaScript.extractOrt(text)
+        self.assertEqual(sachverhalt, "")
+        self.assertEqual(ort, "")
+
+    def test_createThreemaMessage(self):
+        sachverhalt = "Achtung es brennt"
+        ort = "Schweinestall 7 10000 Bauernhof"
+        message = threemaScript.createThreemaMessage(sachverhalt, ort)
+        self.assertEqual(message, """Achtung es brennt
+Schweinestall 7 10000 Bauernhof""")
 
 if __name__ == '__main__':
     unittest.main()
